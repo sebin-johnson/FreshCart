9a57ca04010810b611a46b0b04c51d8b20d18074
sebin005 <sebinvalloor26@gmail.com>
2025-06-26 20:00:13 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://fresh-cart-phi-six.vercel.app'];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // ✅ App routes
